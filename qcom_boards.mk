3b4f5e36d56d81db63fba870d405bf5596631e2a
Alexander Koskovich <zvnexus@outlook.com>
2020-11-16 10:36:10 -0700

    common: Add families for kernel versions
     * Wanted by some component checks, inspired by the way
       LOS handles families.
    
    Change-Id: I63f0367ee7d6a1372c5d392bddbd3ec1153680a1

--- qcom_boards.mk
+++ qcom_boards.mk
@@ -60,3 +60,62 @@ MASTER_SIDE_CP_TARGET_LIST := \
 
 # Refresh list of targets.
 -include vendor/qcom/opensource/core-utils/build/utils.mk
+
+# Kernel Families
+5_15_FAMILY := \
+    bengal_515 \
+    crow \
+    kona_515 \
+    kalama \
+    monaco
+
+5_10_FAMILY := \
+    parrot \
+    taro
+
+5_4_FAMILY := \
+    holi \
+    lahaina
+
+4_19_FAMILY := \
+    bengal \
+    kona \
+    lito
+
+4_14_FAMILY := \
+    $(MSMSTEPPE) \
+    $(TRINKET) \
+    atoll \
+    msmnile
+
+4_9_FAMILY := \
+    msm8953 \
+    qcs605 \
+    sdm710 \
+    sdm845
+
+4_4_FAMILY := \
+    msm8998 \
+    sdm660
+
+3_18_FAMILY := \
+    msm8937 \
+    msm8996
+
+ifeq ($(call is-board-platform-in-list,$(5_15_FAMILY)),true)
+TARGET_KERNEL_VERSION ?= 5.15
+else ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY)),true)
+TARGET_KERNEL_VERSION ?= 5.10
+else ifeq ($(call is-board-platform-in-list,$(5_4_FAMILY)),true)
+TARGET_KERNEL_VERSION ?= 5.4
+else ifeq ($(call is-board-platform-in-list,$(4_19_FAMILY)),true)
+TARGET_KERNEL_VERSION ?= 4.19
+else ifeq ($(call is-board-platform-in-list,$(4_14_FAMILY)),true)
+TARGET_KERNEL_VERSION ?= 4.14
+else ifeq ($(call is-board-platform-in-list,$(4_9_FAMILY)),true)
+TARGET_KERNEL_VERSION ?= 4.9
+else ifeq ($(call is-board-platform-in-list,$(4_4_FAMILY)),true)
+TARGET_KERNEL_VERSION ?= 4.4
+else ifeq ($(call is-board-platform-in-list,$(3_18_FAMILY)),true)
+TARGET_KERNEL_VERSION ?= 3.18
+endif
