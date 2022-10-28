8049e9abf60cc2236a10b6ee23d56d2f9f56e63f
Jake Weinstein <jake@aospa.co>
2022-10-29 02:17:31 +0900

    common: telephony: Use legacy IPA for pre-5.10 devices
    Change-Id: Ifcc13f9531e174157852cda0549ec350fc5f67bd

--- telephony/qti-telephony.mk
+++ telephony/qti-telephony.mk
@@ -30,7 +30,13 @@ $(call inherit-product, vendor/qcom/opensource/dataservices/dataservices_vendor_
 SYSTEM_EXT_MANIFEST_FILES += device/qcom/common/telephony/framework_manifest.xml
 
 # IPACM
+ifneq (,$(filter 5.10, $(TARGET_KERNEL_VERSION)))
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/data-ipa-cfg-mgr
 $(call inherit-product, vendor/qcom/opensource/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
+else
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/data-ipa-cfg-mgr-legacy
+$(call inherit-product, vendor/qcom/opensource/data-ipa-cfg-mgr-legacy/ipacm_vendor_product.mk)
+endif
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
